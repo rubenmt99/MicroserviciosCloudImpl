161ba6c2b6c39cadb034ea04fab51af5c0aa67d3
Ruben Matias Tiscar <rmatiast@emeal.nttdata.com>
2023-03-27 15:56:17 +0200

    Initial commit

---
+++ src/main/java/com/example/microservicioscloudimpl/MicroserviciosCloudImplApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicioscloudimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCloudImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviciosCloudImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicioscloudimpl/MicroserviciosCloudImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicioscloudimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosCloudImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
